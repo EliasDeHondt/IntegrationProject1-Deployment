c3b8cba78ee9635fbe2b8084f0ab75c749646341
EliasDeHondt <Elias.dehondt@outlook.com>
2024-02-23 21:05:24 +0100

    Update GitLab to GitHub integration scripts

--- tools/GitLab to GitHub Integrationproject1.bat
+++ tools/GitLab to GitHub Integrationproject1.bat
@@ -2,13 +2,13 @@
 
 mkdir lokalerepo
 cd lokalerepo
-git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/development
+git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/development.git/
 cd development
-git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Development.git
+git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Development.git/
 cd ..
-git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/deployment
+git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/deployment.git/
 cd development
-git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Deployment.git
+git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Deployment.git/
 cd ..
 cd ..
 rmdir /s /q lokalerepo
