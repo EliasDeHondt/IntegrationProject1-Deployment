2da3c4a6159df2a68d6c871e5787598df7dc18df
EliasDeHondt <Elias.dehondt@outlook.com>
2024-02-24 20:58:12 +0100

    Remove GitLab to GitHub Integration script

--- Tools/GitLab to GitHub Integrationproject1.ps1
+++ Tools/GitLab to GitHub Integrationproject1.ps1
@@ -7,7 +7,7 @@ cd development
 git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Development.git/
 cd ..
 git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/deployment.git/
-cd development
+cd deployment
 git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Deployment.git/
 cd ..
 cd ..
