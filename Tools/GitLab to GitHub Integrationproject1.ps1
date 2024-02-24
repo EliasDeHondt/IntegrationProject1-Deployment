932a6ce5417b13604079dd3c0f596f42cf05a375
EliasDeHondt <Elias.dehondt@outlook.com>
2024-02-24 21:09:28 +0100

    Update GitLab to GitHub integration script

--- Tools/GitLab to GitHub Integrationproject1.ps1
+++ Tools/GitLab to GitHub Integrationproject1.ps1
@@ -1,12 +1,41 @@
-mkdir lokalerepo
-cd lokalerepo
+############################
+# @author Elias De Hondt   #
+# @see https://eliasdh.com #
+# @since 01/02/2024        #
+############################
+
+# Create a new directory
+New-Item -ItemType Directory -Path "localrepo"
+
+# Navigate to the new directory
+Set-Location "localrepo"
+
+# Clone the repository
 git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/development.git/
-cd development
+
+# Navigate to the directory
+Set-Location "development"
+
+# Push the repository to the GitHub mirror
 git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Development.git/
-cd ..
+
+# Navigate back to the parent directory
+Set-Location ..
+
+# Clone the repository
 git clone https://gitlab.com/kdg-ti/integratieproject-1/202324/23_codeforge/deployment.git/
-cd deployment
+
+# Navigate to the directory
+Set-Location "deployment"
+
+# Push the repository to the GitHub mirror
 git push --mirror https://github.com/EliasDeHondt/IntegrationProject1-Deployment.git/
-cd ..
-cd ..
-rmdir /s /q lokalerepo
+
+# Navigate back to the parent directory
+Set-Location ..
+
+# Go up one level
+Set-Location ..
+
+# Remove the local repository
+Remove-Item -Recurse -Force "localrepo"
