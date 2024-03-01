2fa26126a802c9fa6fbb4841bf2d988d73e726a9
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-01 20:18:29 +0100

    Fix root user check and add Google Cloud CLI check

--- Scripts/Delete-Infrastructure.sh
+++ Scripts/Delete-Infrastructure.sh
@@ -10,16 +10,22 @@ rood='\e[0;31m'
 blauw='\e[0;34m'
 groen='\e[0;32m'
 
-# Functie: Error afhandeling
+# Functie: Error afhandeling.
 function error_exit() {
   echo -e "Error: ${rood}$1${reset}"
   exit 1
 }
 
 # Functie: Check of de script als root wordt uitgevoerd.
-[ "$EUID" -ne 0 ] && error_exit "Script must be run as root, e.g., using sudo." 
+[ "$EUID" -ne 0 ] && error_exit "Script must be run as root: sudo $0"
+
+# Functie: Check if the Google Cloud CLI is installed.
+[ ! command -v gcloud &> /dev/null ] then
+  error_exit "Google Cloud CLI is not installed. Please install it before running this script."
+fi
 
 # Functie: Print the welcome message.
+clear
 echo -e "${blauw}*********************************************"
 echo "*                                           *"
 echo "*     Running CodeForge delete script.      *"
