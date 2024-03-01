fdfa8c404e9202282937c355e05806b2b525c171
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-01 15:25:50 +0100

    Update user authentication logic

---
+++ Scripts/Create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+######################
+# Van Elias De Hondt #
+######################
+# This is de script to create the GCloud infrastructure.
---
+++ Scripts/Delete.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+######################
+# Van Elias De Hondt #
+######################
+# This is de script to delete the GCloud infrastructure.
